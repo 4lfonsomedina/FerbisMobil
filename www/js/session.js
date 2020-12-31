21ba125af696e4d1b1b851ed47e98ac3a2b9bc9d
4lfonsomedina <4lfonsomedina@gmail.com>
2020-12-31 11:19:26 -0800

    prueba push

--- www/js/session.js
+++ www/js/session.js
@@ -64,3 +64,35 @@ $(document).ready(function() {
 		}
 	})
 });
+
+
+
+
+// Add to index.js or the first page that loads with your app.
+// For Intel XDK and please add this to your app.js.
+
+document.addEventListener('deviceready', function () {
+  //Remove this method to stop OneSignal Debugging 
+  window.plugins.OneSignal.setLogLevel({logLevel: 6, visualLevel: 0});
+  
+  var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+  // Set your iOS Settings
+  var iosSettings = {};
+  iosSettings["kOSSettingsKeyAutoPrompt"] = false;
+  iosSettings["kOSSettingsKeyInAppLaunchURL"] = false;
+  
+  window.plugins.OneSignal
+    .startInit("82e5a9a1-634d-4c23-8629-0524c9fc5379")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .iOSSettings(iosSettings)
+    .inFocusDisplaying(window.plugins.OneSignal.OSInFocusDisplayOption.Notification)
+    .endInit();
+  
+  // The promptForPushNotificationsWithUserResponse function will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 6)
+  window.plugins.OneSignal.promptForPushNotificationsWithUserResponse(function(accepted) {
+    console.log("User accepted notifications: " + accepted);
+  });
+  
+}, false);
